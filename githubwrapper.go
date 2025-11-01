a0ebe9388ed761185fed6c34cecf615730b8c4bd
pirespsps <matheusknoll28@gmail.com>
2025-11-01 16:52:59 -0300

    Formatação em mês funcionando parcialmente

--- githubwrapper.go
+++ githubwrapper.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Query struct {
@@ -21,7 +24,7 @@ type Query struct {
 	}
 }
 
-func getCommits(user string) Query {
+func getCommits(user string) map[int][]bool {
 
 	var url = "https://api.github.com/search/commits?q=author:" + user + "&merge=true"
 
@@ -39,25 +42,60 @@ func getCommits(user string) Query {
 
 	json.Unmarshal(data, &commits)
 
-	return commits
+	return formatByMonths(&commits)
 
 }
 
-func formatByMonths(*Query) {
+func formatByMonths(query *Query) map[int][]bool {
 
-	type Month struct {
-		Month  int
-		Commit struct {
-			Message    string
-			Date       string
-			Repository string
+	months := make(map[int][]bool)
+
+	var currentMonth int
+	var monthCheck = make([]bool, 31)
+
+	for _, v := range query.Items {
+
+		month, err := strconv.Atoi(v.Commit.Author.Date[5:7])
+		if err != nil {
+			log.Fatal(err)
 		}
+
+		day, err := strconv.Atoi(v.Commit.Author.Date[8:10])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("mes: %v dia: %v\n", month, day)
+
+		//está começando com o primeiro mes inteiro zerado devido a inicialização
+		//não está verificando mês a mês, apenas os que tem pelo menos uma commit
+		//fazer um for para ir zerando até chegar na primeira commit
+		//mudar a query da api para só puxar do ano
+		if month != currentMonth {
+			totalDays := daysIn(time.Month(currentMonth), time.Now().Year())
+
+			for i := range totalDays {
+
+				if monthCheck[i] {
+					continue
+				}
+				monthCheck[i] = false
+			}
+
+			months[currentMonth] = monthCheck
+			monthCheck = make([]bool, 31)
+			currentMonth = month
+
+		}
+
+		monthCheck[day] = true
+
 	}
 
-	var months = []Month{}
+	return months
 
-	for _, v := range &Query.Items {
+}
 
-	}
-
+func daysIn(m time.Month, year int) int {
+	return time.Date(year, m+1, 0, 0, 0, 0, 0, time.UTC).Day()
 }
